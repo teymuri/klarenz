81233067a416490a2237d461752a2dc8e94ae1d1
Amir Teymuri <amir@bunsenlabs.smt>
2020-12-12 00:16:12 +0100

    rm mail

--- __init__.py
+++ __init__.py
@@ -11,7 +11,6 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License at <http://www.gnu.org/licenses/>
 for more details.
 Copyright (c) 2018, 2019 Amir Teymuri
-<ateymuri63@gmail.com>
 """
 
 from .kodou import kodou
