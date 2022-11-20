c795034321cbef6c6923bd2b24ca6c4a06cad96e
Amir Teymuri <ateymuri63@gmail.com>
2022-11-20 22:49:20 +0100

    Check if kodourc keys r valid befor going for processing
    This happened to me I had lily_bin instead of ly_bin, and was looking
    for the problem couple of minutes...

--- test.py
+++ test.py
@@ -11,13 +11,21 @@ from kodou import *
 #            # }}
 #           ))
 
-kodou(Part(events={"notes": range(60, 84),
-                   "beats": range(24)},
-           metadata={"clef": {
-               (0.5, 8+1/3): "alto",
-               (4, 6): "tenor",
-               8+2/3: "treble+9",
-               13.5: "treble-8",
-               16: "soprano"
-           }}))
-
+# kodou(Part(events={"notes": range(60, 84),
+#                    "beats": range(24)},
+#            metadata={"clef": {
+#                (0.5, 8+1/3): "alto",
+#                (4, 6): "tenor",
+#                8+2/3: "treble+9",
+#                13.5: "treble-8",
+#                16: "soprano"
+#            }}))
+def b():
+    i = 0
+    while True:
+        yield 60 + 12 * (i % 4)
+        i += 1
+B = b()
+print([next(B) for _ in range(10)])
+kodou(Part(events={"notes": [next(B) for _ in range(53)],
+                   "beats": range(3, 480, 9)}))
