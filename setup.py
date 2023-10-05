037f19809f038ab30043db0afe526040f970a6ae
Amir Teymuri <amiratwork22@gmail.com>
2023-10-05 11:57:35 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import find_packages
 from setuptools import setup
 
 
 setup(
-	name='klarenz',
-	version='1.0.0',
-	description='Programmatically generate Lilypond scores, with ease and elegance of pure Python ',
-	author='Amir Teymuri',
-	author_email='amiratwork22@gmail.com',
-	packages=['klarenz'],
-    # packages=find_packages('src'),
-    package_dir={'klarenz': 'src'},
+	name = 'klarenz',
+	version = '1.0.0',
+	description = 'Programmatically generate Lilypond scores, with ease and elegance of pure Python ',
+	author = 'Amir Teymuri',
+	author_email = 'amiratwork22@gmail.com',
+	packages = ['klarenz'],
+    package_dir = {'klarenz': 'src'},
+    url = 'https://github.com/teymuri/klarenz.git',
+    python_requires = ">=3.5"
 )
 
