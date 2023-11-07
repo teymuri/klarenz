49ecd8cce9945debc8ba64519fe1a677205214be
Amir Teymuri <amiratwork22@gmail.com>
2023-11-07 11:51:43 +0100

    Reformat ly

--- src/ly.py
+++ src/ly.py
@@ -2,17 +2,17 @@
 # polymetric notation
 UNEQUAL_LENGTH_MEASURES_POLYMETRY = """
 \layout {
-  \context {
-    \Score
-    \\remove "Timing_translator"
-    \\remove "Default_bar_line_engraver"
-  }
-  \context {
-    \Staff
-    \consists "Timing_translator"
-    \consists "Default_bar_line_engraver"
-    \RemoveEmptyStaves
-  }
+\context {
+\Score
+\\remove "Timing_translator"
+\\remove "Default_bar_line_engraver"
+}
+\context {
+\Staff
+\consists "Timing_translator"
+\consists "Default_bar_line_engraver"
+\RemoveEmptyStaves
+}
 }
 """
 
@@ -24,14 +24,14 @@ tagline = ##f
 
 PAPER = """
 \paper {
-	left-margin = 20
-	right-margin = 20
-	score-system-spacing = #'( (basic-distance . 10) (minimum-distance . 10) (padding . 10) (stretchability . 0) )
-	system-system-spacing = #'( (basic-distance . 0) (minimum-distance . 0) (padding . 10) (stretchability . 0) )
-	top-margin = 20
-	bottom-margin = 20
-	indent = 0
-    %% entfernt die Trennline oberhalb der Fußnotentexte am Seitenende
-    footnote-separator-markup = ##f  
+left-margin = 20
+right-margin = 20
+score-system-spacing = #'( (basic-distance . 10) (minimum-distance . 10) (padding . 10) (stretchability . 0) )
+system-system-spacing = #'( (basic-distance . 0) (minimum-distance . 0) (padding . 10) (stretchability . 0) )
+top-margin = 20
+bottom-margin = 20
+indent = 0
+%% entfernt die Trennline oberhalb der Fußnotentexte am Seitenende
+footnote-separator-markup = ##f  
 }
 """
