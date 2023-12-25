16680e03779a445a50cafbc554b69d014bdf1b91
Amir Teymuri <amiratwork22@gmail.com>
2023-12-25 20:08:56 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 
 setup(
 	name = 'klarenz',
-	version = '0.0.4',
+	version = '0.0.5',
 	description = 'Programmatically generate Lilypond scores, with ease and elegance of pure Python ',
 	author = 'Amir Teymuri',
 	author_email = 'amiratwork22@gmail.com',
 	packages = ['klarenz'],
-    package_dir = {'klarenz': 'src'},
+    package_dir = {'klarenz': 'src/klarenz'},
     url = 'https://github.com/teymuri/klarenz.git',
     python_requires = ">=3.5",
-    install_requires = ["toml"]
 )
 
