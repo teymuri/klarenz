257d699be4ba51581275aac2e7db685cd36878cd
Amir Teymuri <amiratwork22@gmail.com>
2023-12-27 11:05:47 +0100

    Update readme version information with prebuild

--- prebuild.py
+++ prebuild.py
@@ -28,3 +28,18 @@ classifiers = [
 
 with open("pyproject.toml", "w") as f:
     f.write(toml)
+
+
+readme = f"""
+[__Klarenz__](https://en.wikipedia.org/wiki/Clarence_Barlow) is a highly minimalist (the entire API consists of a single class `Part` and a main processor function `proc`!) and Pythonic package for compiling Lilypond sheet music.
+
+For more information check the [documentation page](https://teymuri.github.io/klarenz-docs/).
+
+# Current release: v{version}
+
+# Packaging
+Ensure to update the `src/klarenz/version.py` file and run `python prebuild.py` (and push them!) before initiating the build-upload process for each release.
+"""
+
+with open("README.md", "w") as f:
+    f.write(readme)
